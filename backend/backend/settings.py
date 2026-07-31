cd50258d682bcbb8aa6fd143fe90029299603347
Yusuf-creator868 <yakubovyusuf821@gmail.com>
2026-07-31 21:14:55 +0500

    Changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,11 +131,13 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://wanderly-ebon.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://wanderly-ebon.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
