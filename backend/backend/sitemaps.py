4f06e8af1797e6d69b908d9bb27321f65c06ac0f
Yusuf-creator868 <yakubovyusuf821@gmail.com>
2026-08-04 00:39:53 +0500

    Changes

--- backend/backend/sitemaps.py
+++ backend/backend/sitemaps.py
@@ -1,12 +1,10 @@
 from django.contrib.sitemaps import Sitemap
 from agency.models import Tour
-from django.conf import settings
 
 
 class StaticViewSitemap(Sitemap):
     priority = 1.0
     changefreq = "weekly"
-    protocol = "https"
 
     def items(self):
         return [
@@ -19,17 +17,10 @@ class StaticViewSitemap(Sitemap):
     def location(self, item):
         return item
 
-    def get_urls(self, page=1, site=None, protocol=None):
-        urls = super().get_urls(page=page, site=site, protocol=protocol)
-        for url in urls:
-            url["location"] = f"{settings.SITE_URL}{url['location']}"
-        return urls
-
 
 class TourSitemap(Sitemap):
     priority = 0.9
     changefreq = "daily"
-    protocol = "https"
 
     def items(self):
         return Tour.objects.filter(status="published")
@@ -39,9 +30,3 @@ class TourSitemap(Sitemap):
 
     def location(self, obj):
         return f"/details/{obj.pk}"
-
-    def get_urls(self, page=1, site=None, protocol=None):
-        urls = super().get_urls(page=page, site=site, protocol=protocol)
-        for url in urls:
-            url["location"] = f"{settings.SITE_URL}{url['location']}"
-        return urls
