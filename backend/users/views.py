4e6ac74cbc65978a96b1118e5e105fcdee9692a8
Yusuf-creator868 <yakubovyusuf821@gmail.com>
2026-08-04 18:47:10 +0500

    Changes

--- backend/users/views.py
+++ backend/users/views.py
@@ -372,6 +372,9 @@ def get_favorites(request):
 @api_view(["GET"])
 def get_tours(request):
     fav_code = request.query_params.get("fav_code")
+    query = request.GET.get("q", "")
+    print("RAW QUERY REPR:", repr(query))
+    print("RAW QUERY BYTES:", query.encode('utf-8', errors='replace'))
 
     favorite_qs = FavoriteItems.objects.filter(
         favcart__fav_code=fav_code,
